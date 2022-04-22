3c79a440a6126e360ce56f4fc4c0601cd02f3eb9
alexmorales33 <alexmoraleess4@gmail.com>
2022-04-22 02:24:08 -0300

    initial commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import './footer.scss';
+
+import { Link } from 'react-router-dom';
+
+import bg from '../../assets/footer-bg.jpg';
+import logo from '../../assets/logo.png';
+
+const Footer = () => {
+    return (
+        <div className="footer" style={{backgroundImage: `url(${bg})`}}>
+            <div className="footer__content container">
+                <div className="footer__content__logo">
+                    <div className="logo">
+                        <Link to="/"><img src={logo} alt="" /></Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
